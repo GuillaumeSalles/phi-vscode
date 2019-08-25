45fd13d74b0aff046531e3c07cd9b1a017fe3260
Guillaume Salles <guillaume.salles@me.com>
2019-08-25 17:47:26 -0400

    No color support

--- studio/src/components/ColorInput.tsx
+++ studio/src/components/ColorInput.tsx
@@ -3,29 +3,34 @@ import { jsx } from "@emotion/core";
 import * as T from "../types";
 import Select from "./Select";
 import { getContrastColor } from "../utils";
-import { firstEntry } from "../helpers/immutable-map";
 
 type Props = {
   value?: T.Color;
   colors: T.ColorsMap;
-  onChange: (color: T.Color) => void;
+  onChange: (color: T.Color | undefined) => void;
 };
 
 function ColorInput({ value, colors, onChange }: Props) {
   if (value != null && value.type === "hex") {
     throw new Error("");
   }
-  const colorId = value != null ? value.id : firstEntry(colors)[0];
+  const colorId = value != null ? value.id : "none";
+  const options = ([["none", "none"]] as [string, string][]).concat(
+    Array.from(colors.entries()).map(entry => [entry[0], entry[1].name])
+  );
+  const backgroundColor =
+    value != null ? colors.get(colorId)!.value : "transparent";
+  const contrastColor =
+    value != null ? getContrastColor(colors.get(colorId)!.value) : "black";
   return (
     <Select
       value={colorId}
-      options={Array.from(colors.entries()).map(entry => [
-        entry[0],
-        entry[1].name
-      ])}
-      onChange={value => onChange({ type: "ref", id: value })}
-      backgroundColor={colors.get(colorId)!.value}
-      color={getContrastColor(colors.get(colorId)!.value)}
+      options={options}
+      onChange={value =>
+        onChange(value == "none" ? undefined : { type: "ref", id: value })
+      }
+      backgroundColor={backgroundColor}
+      color={contrastColor}
     />
   );
 }
