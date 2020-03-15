ca2c249e695509dc17ba41ce9c33b133ae68adcf
Guillaume Salles <guillaume.salles@me.com>
2020-03-15 12:25:17 -0400

    Remove layout from gatsby sandbox

--- sandboxes/gatsby-site/src/pages/index.js
+++ sandboxes/gatsby-site/src/pages/index.js
@@ -1,24 +1,19 @@
 import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import { Profile, Tmp } from "../Twitter.phi"
+import { Profile } from "../Twitter.phi"
 
 import ReactProfile from "../images/ReactProfile.png"
 import ReactBackground from "../images/ReactBackground.jpeg"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Profile
-      profileImage={ReactProfile}
-      backgroundImage={ReactBackground}
-      name="React"
-      handle="@reactjs"
-      description="React is a declarative, efficient, and flexible JavaScript library for building user interfaces."
-      followers="331.6K"
-      following="260"
-    />
-  </Layout>
+  <Profile
+    profileImage={ReactProfile}
+    backgroundImage={ReactBackground}
+    name="React"
+    handle="@reactjs"
+    description="React is a declarative, efficient, and flexible JavaScript library for building user interfaces."
+    followers="331.6K"
+    following="260"
+  />
 )
 
 export default IndexPage
