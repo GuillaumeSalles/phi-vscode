6c28669962d2418c64af6b76c157ec0e4cf9461e
Guillaume Salles <guillaume.salles@me.com>
2020-03-15 12:28:23 -0400

    Gatsby sandbox improvements

--- sandboxes/gatsby-site/src/pages/index.js
+++ sandboxes/gatsby-site/src/pages/index.js
@@ -3,17 +3,29 @@ import { Profile } from "../Twitter.phi"
 
 import ReactProfile from "../images/ReactProfile.png"
 import ReactBackground from "../images/ReactBackground.jpeg"
+import GatsbyProfile from "../images/GatsbyProfile.png"
+import GatsbyBackground from "../images/GatsbyBackground.jpeg"
 
 const IndexPage = () => (
-  <Profile
-    profileImage={ReactProfile}
-    backgroundImage={ReactBackground}
-    name="React"
-    handle="@reactjs"
-    description="React is a declarative, efficient, and flexible JavaScript library for building user interfaces."
-    followers="331.6K"
-    following="260"
-  />
+  <>
+    <Profile
+      profileImage={ReactProfile}
+      backgroundImage={ReactBackground}
+      name="React"
+      handle="@reactjs"
+      description="React is a declarative, efficient, and flexible JavaScript library for building user interfaces."
+      followers="331.6K"
+      following="260"
+    />
+    <Profile
+      profileImage={GatsbyProfile}
+      backgroundImage={GatsbyBackground}
+      name="Gatsby"
+      handle="@gatsbyjs"
+      description="Build blazing fast, modern apps and websites with React"
+      following="35"
+      followers="48.7K" />
+  </>
 )
 
 export default IndexPage
